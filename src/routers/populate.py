97834cecaec57a3cb0d8e95e6cb1f59ba2495c23
vnascimento <victor.nascimento@encora.com>
2023-08-13 21:09:34 -0300

    add pets no populate

--- src/routers/populate.py
+++ src/routers/populate.py
@@ -5,7 +5,7 @@ from loguru import logger
 from sqlalchemy.orm import Session
 
 from src import populate_namespace as app
-from src.models import User
+from src.models import Pet, User
 from src.routers.helpers import get_response, configure_session
 
 
@@ -15,6 +15,7 @@ class RoutePopulate(Resource):
         with closing(configure_session()) as session:
             try:
                 create_user(session)
+                create_pets(session)
             except Exception as e:
                 msg = f'Unable to populate database. Rollback executed: {str(e)}'
                 session.rollback()
@@ -38,4 +39,41 @@ def create_user(session: Session):
 
     if not user:
         session.add(User(email, name, lastname, document, phone_number, pwd))
+        session.commit()
+
+
+def create_pets(session: Session):
+    
+    user_id = session.query(User.id).filter(User.activated).filter(User.email == 'admin@admin.com').scalar()
+
+    pet: Pet = session.query(Pet).filter(Pet.activated).all()
+
+    if not pet:
+        pets = [
+            {
+                'name': 'Lily',
+                'size': 'medio',
+                'breed': 1,
+                'age': '3 anos',
+                'castrated': True,
+                'weight': '30 kg',
+                'specie': 'cachorro',
+                'gender': 'femea',
+                'user_id': user_id,
+            },
+            {
+                'name': 'Nick',
+                'size': 'pequeno',
+                'breed': 2,
+                'age': '5 anos',
+                'castrated': False,
+                'weight': '3 kg',
+                'specie': 'gato',
+                'gender': 'macho',
+                'user_id': user_id,
+            },
+        ]
+        
+        for pet in pets:
+            session.add(Pet(pet['name'], pet['size'], pet['breed'], pet['age'], pet['castrated'], pet['weight'], pet['specie'], pet['gender'], pet['user_id']))
         session.commit()
